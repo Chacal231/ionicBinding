e08449405d317ac69f7dd11d28238f322fbbeffa
Chacal231 <daniel.extra231@gmail.com>
2026-09-23 17:09:42 +0200

    template ionic

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../usuario';
+import { Servicio } from '../servicio';
+import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonToolbar, IonTitle } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
   styleUrls: ['./galeria.component.scss'],
-  imports: [],
+  imports: [IonHeader, IonToolbar, IonContent, IonList, IonItem, IonLabel, IonTitle, CommonModule, IonItem],
 })
-export class GaleriaComponent  implements OnInit {
+export class GaleriaComponent implements OnInit {
 
-  public tituloSeccion: string= 'Galeria de Imágenes Dinámica';
+  public tituloSeccion: string = 'Galeria de Imágenes Dinámica';
   public description: string = 'Esta imagen se carga desde el directorio de assets mediante Propery Binding';
-  public rutaImagenLocal: string ='assets/foto1.jpg';
+  public rutaImagenLocal: string = 'assets/foto1.jpg';
 
-  constructor() { }
+  users: Usuario[] = [];
 
-  ngOnInit() {}
+  constructor(private usersService: Servicio) { }
+
+  async ngOnInit() {
+    this.users = await this.usersService.getUsuarios();
+    console.log(this.users);
+    console.log("prueba");
+  }
 
 }
--- src/app/servicio.ts
+++ src/app/servicio.ts
@@ -1,7 +1,11 @@
-import { Service } from '@angular/core';
+import { Injectable, Service } from '@angular/core';
 import { Usuario } from './usuario';
 
-@Service()
+@Injectable({
+    providedIn:'root'
+})
+
+
 export class Servicio {
 
     private users: Usuario[] = [
@@ -10,7 +14,7 @@ export class Servicio {
         { id: 3, name: 'maria', email: 'maria@gmail.com', active: true },
     ];
 
-    async getUsuarios(): Promise<Usuario> {
+    async getUsuarios(): Promise<Usuario[]> {
         return new Promise(resolve => { setTimeout(() => this.users, 1000); });
     }
 
